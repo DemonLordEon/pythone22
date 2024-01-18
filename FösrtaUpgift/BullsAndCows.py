8e619bf1470995694a43084c487261d4a635f76d
elis laxvik <drpokeguy1@gmail.com>
2024-01-18 16:38:07 +0100

    Bulls And Cows

---
+++ FösrtaUpgift/BullsAndCows.py
@@ -0,0 +1,34 @@
+import random
+
+number = []
+number.append(random.randint(1, 9))
+number.append(random.randint(1, 9))
+number.append(random.randint(1, 9))
+number.append(random.randint(1, 9))
+
+print("****")
+
+guess = None
+
+
+
+
+while guess != number:
+    print("guess the 4 digit number")
+    temp = input()
+    guess = []
+    for x in temp:
+        guess.append(int(x))
+    
+    for i in range(len(number)):
+        
+        print(number[i] == guess[i])
+        if (guess[i] == number[i]): 
+            print("number is " + str(i + 1) + " is correct.")
+        else:
+            for j in range(len(number)):
+                if (i != j):
+                    if (guess[i] == number[j]):
+                        print("number " + str(i + 1) + " exist in another positon.")
+guess = number 
+print("You win")
